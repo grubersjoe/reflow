6332bbaaca65142ae19313f16d89931155566dc9
Jonathan Gruber <jogu@spreadshirt.net>
2019-03-21 15:26:43 +0100

    Support for recursive fixture directories

--- src/plugin/__tests__/index.test.ts
+++ src/plugin/__tests__/index.test.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { getTransformOptions } from '../options';
 import { runFixtureTests } from '../../util/test';
 
-runFixtureTests({
-  babelOptions: getTransformOptions(undefined, { comments: false }),
-  fixturesRoot: path.resolve('src/plugin/__tests__/__fixtures__/'),
-});
+const rootDir = path.resolve('src/plugin/__tests__/__fixtures__/');
+const babelOptions = getTransformOptions(undefined, { comments: false });
+
+runFixtureTests(rootDir, babelOptions);
--- src/util/test.ts
+++ src/util/test.ts
@@ -6,60 +6,59 @@ import path from 'path';
 
 import { splitFixtureLines } from './string';
 
-export interface FixtureTestRunnerArgs {
-  babelOptions: TransformOptions;
-  fixturesRoot: string;
-  inputFilename?: string;
-  outputFilename?: string;
-}
+const FIXTURE_INPUT_FILENAME = 'input.js';
+const FIXTURE_OUTPUT_FILENAME = 'output.ts';
 
-export const defaultFixtureRunnerArgs = {
-  inputFilename: 'input.js',
-  outputFilename: 'output.ts',
-};
+export function runFixtureTests(rootDir: string, babelOptions: TransformOptions): void {
+  const files = fs.readdirSync(rootDir);
 
-export function runFixtureTests(args: FixtureTestRunnerArgs): void {
-  const mergedArgs = Object.assign({}, defaultFixtureRunnerArgs, args);
-  const { babelOptions, fixturesRoot, inputFilename, outputFilename } = mergedArgs;
+  if (files.length === 0) {
+    throw new Error(`No fixture directories or files found in ${rootDir}`);
+  }
 
-  fs.readdirSync(fixturesRoot).forEach(dirName => {
-    const dir = path.join(fixturesRoot, dirName);
+  files.forEach(dirName => {
+    const dir = path.resolve(rootDir, dirName);
 
     if (statSync(dir).isDirectory()) {
       const testName = startCase(dirName);
-      const inputFilePath = path.join(dir, inputFilename);
-      const outputFilePath = path.join(dir, outputFilename);
 
-      if (!fs.existsSync(inputFilePath)) {
-        throw new Error(`Fixture input file ${inputFilePath} does not exist.`);
-      }
+      const inputFile = path.resolve(dir, FIXTURE_INPUT_FILENAME);
+      const outputFile = path.resolve(dir, FIXTURE_OUTPUT_FILENAME);
 
-      if (!fs.existsSync(outputFilePath)) {
-        throw new Error(`Fixture output file ${outputFilePath} does not exist.`);
-      }
+      const inputFileExists = fs.existsSync(inputFile);
+      const outputFileExists = fs.existsSync(outputFile);
 
-      const input = fs.readFileSync(inputFilePath).toString();
-      const babelOutput = transformSync(input, babelOptions);
-      const expectedLines = splitFixtureLines(fs.readFileSync(outputFilePath));
+      if (inputFileExists && outputFileExists) {
+        const input = fs.readFileSync(inputFile).toString();
+        const babelOutput = transformSync(input, babelOptions);
+        const expectedLines = splitFixtureLines(fs.readFileSync(outputFile));
 
-      describe(testName.toUpperCase(), () => {
-        if (!babelOutput) {
-          throw new Error(`Unable to transform file ${inputFilePath}.`);
-        }
+        describe(testName.toUpperCase(), () => {
+          if (!babelOutput) {
+            throw new Error(`Unable to transform file ${inputFile}.`);
+          }
 
-        if (babelOutput.code) {
-          splitFixtureLines(babelOutput.code).forEach((line, i) => {
-            const padLength = Math.min(String(expectedLines.length).length, 2);
-            const number = String(i + 1).padStart(padLength, '0');
+          if (babelOutput.code) {
+            splitFixtureLines(babelOutput.code).forEach((line, i) => {
+              const padLength = Math.min(String(expectedLines.length).length, 2);
+              const testNumber = String(i + 1).padStart(padLength, '0');
 
-            test(`${testName}:${number} │ ${line}  ===  ${expectedLines[i]}`, () => {
-              expect(line).toMatch(expectedLines[i]);
+              test(`${testName}:${testNumber} │ ${line}  ===  ${expectedLines[i]}`, () => {
+                expect(line).toMatch(expectedLines[i]);
+              });
             });
-          });
-        } else {
-          console.warn(chalk.yellow(`Input file ${inputFilePath} seems to be empty.`));
-        }
-      });
+          } else {
+            console.warn(chalk.yellow(`Input file ${inputFile} seems to be empty.`));
+          }
+        });
+      } else if (inputFileExists) {
+        throw new Error(`Fixture output file ${outputFile} does not exist.`);
+      } else if (outputFileExists) {
+        throw new Error(`Fixture input file ${inputFile} does not exist.`);
+      } else {
+        // Descend in next directory level
+        runFixtureTests(dir, babelOptions);
+      }
     }
   });
 }
