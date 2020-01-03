16d4e43d5b626d5b0cff2d245b713053f112c65d
Jonathan Gruber <gruberjonathan@gmail.com>
2020-01-03 22:13:18 +0100

    Fix TS error
    Introduced with TS 3.7

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,7 +1,6 @@
-import { ParserOptions, PluginObj, TransformOptions } from '@babel/core';
+import { ParserOptions, TransformOptions } from '@babel/core';
 
 import { getParserPlugins } from './util/options';
-import { PluginPass, VisitorNodes } from './types';
 
 import {
   classDeclarationVisitor,
@@ -17,24 +16,22 @@ export interface ReflowOptions {
   replaceDecorators?: boolean;
 }
 
-function buildPlugin(): () => PluginObj<PluginPass<VisitorNodes>> {
-  return () => ({
-    name: 'reflow',
-    visitor: {
-      ArrowFunctionExpression: functionVisitor,
-      ClassDeclaration: classDeclarationVisitor,
-      FunctionDeclaration: functionVisitor,
-      FunctionExpression: functionVisitor,
-      ImportDeclaration: importDeclarationVisitor,
-      ImportSpecifier: importSpecifierVisitor,
-      Flow: flowVisitor,
-      JSX: jsxVisitor,
-      Program: programVisitor,
-    },
-    manipulateOptions(opts: TransformOptions, parserOpts: ParserOptions) {
-      parserOpts.plugins = getParserPlugins('flow');
-    },
-  });
-}
+const reflowPlugin = {
+  name: 'reflow',
+  visitor: {
+    ArrowFunctionExpression: functionVisitor,
+    ClassDeclaration: classDeclarationVisitor,
+    FunctionDeclaration: functionVisitor,
+    FunctionExpression: functionVisitor,
+    ImportDeclaration: importDeclarationVisitor,
+    ImportSpecifier: importSpecifierVisitor,
+    Flow: flowVisitor,
+    JSX: jsxVisitor,
+    Program: programVisitor,
+  },
+  manipulateOptions(opts: TransformOptions, parserOpts: ParserOptions) {
+    parserOpts.plugins = getParserPlugins('flow');
+  },
+};
 
-export default buildPlugin();
+export default reflowPlugin;
