975f87d0accfd1b71eb9fbb7cbdb775f3fd84a05
Jonathan Gruber <gruberjonathan@gmail.com>
2020-02-27 13:04:37 +0100

    Remove obsolete ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   coverageDirectory: '.coverage',
   coverageReporters: ['lcov', 'text'],
-  preset: 'ts-jest',
   rootDir: '.',
   testRegex: 'src/.*/__tests__/.*.test.ts$',
 };
